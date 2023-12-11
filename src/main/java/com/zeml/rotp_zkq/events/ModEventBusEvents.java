58fe3becf06f6908ec0bf7fad5e64295efcc49f9
StandoByte <94539965+StandoByte@users.noreply.github.com>
2023-12-11 20:49:40 +0300

    Fixed a server crash when summoning SHA

--- src/main/java/com/zeml/rotp_zkq/events/ModEventBusEvents.java
+++ src/main/java/com/zeml/rotp_zkq/events/ModEventBusEvents.java
@@ -3,16 +3,16 @@ package com.zeml.rotp_zkq.events;
 import com.zeml.rotp_zkq.RotpKillerQueen;
 import com.zeml.rotp_zkq.entity.stand.SheerHeart;
 import com.zeml.rotp_zkq.init.ModEntityTypes;
-import net.minecraftforge.api.distmarker.Dist;
+
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = RotpKillerQueen.MOD_ID,bus= Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = RotpKillerQueen.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventBusEvents {
 
     @SubscribeEvent
     public static void addEntityAttribues(EntityAttributeCreationEvent event){
-        event.put(ModEntityTypes.SHEAR_HEART.get(), SheerHeart.createMobAttributes().build());
+        event.put(ModEntityTypes.SHEAR_HEART.get(), SheerHeart.createAttributes().build());
     }
 }
