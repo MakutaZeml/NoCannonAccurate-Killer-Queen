f475a43558c59c9dfffd9eb5059530c037d33ba1
Makuta Zeml <152751407+MakutaZeml@users.noreply.github.com>
2024-02-17 20:22:39 -0600

    Sheer Heart Attack Lag issue solved
    setUpdateInterval reduced to 2

--- src/main/java/com/zeml/rotp_zkq/init/ModEntityTypes.java
+++ src/main/java/com/zeml/rotp_zkq/init/ModEntityTypes.java
@@ -18,14 +18,14 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModEntityTypes {
     public static final DeferredRegister<EntityType<?>> ENTITIES =DeferredRegister.create(ForgeRegistries.ENTITIES, RotpKillerQueen.MOD_ID);
 
-    public static final RegistryObject<EntityType<SnowBombEntity>> SNOW_BOMB = ENTITIES.register("snow_bomb",
+    public static final RegistryObject<EntityType<SnowBombEntity>>  SNOW_BOMB = ENTITIES.register("snow_bomb",
             ()->EntityType.Builder.<SnowBombEntity>of(SnowBombEntity:: new, EntityClassification.MISC).sized(0.15F,0.15F)
                     .setUpdateInterval(10).build(new ResourceLocation(RotpKillerQueen.MOD_ID,"snow_bubble").toString()));
 
 
     public static final RegistryObject<EntityType<SheerHeart>> SHEAR_HEART = ENTITIES.register("sheer_heart",
             ()->EntityType.Builder.<SheerHeart>of(SheerHeart::new, EntityClassification.MISC).sized(0.85F,0.7F)
-                    .setUpdateInterval(10).build((new ResourceLocation(RotpKillerQueen.MOD_ID,"sheer_heart").toString())));
+                    .setUpdateInterval(2).build((new ResourceLocation(RotpKillerQueen.MOD_ID,"sheer_heart").toString())));
 
 
 }
